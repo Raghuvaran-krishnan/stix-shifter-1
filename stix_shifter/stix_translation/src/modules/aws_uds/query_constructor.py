e83804b84e60296e1e33e5df023aae977e62c756
Raghuvaran-krishnan <raghuvaran.krishnan@hcl.com>
2019-11-13 15:43:14 +0530

    aws_uds code backup as on 13/11/2019 before new approach

--- stix_shifter/stix_translation/src/modules/aws_uds/query_constructor.py
+++ stix_shifter/stix_translation/src/modules/aws_uds/query_constructor.py
@@ -1,8 +1,9 @@
 from stix_shifter.stix_translation.src.patterns.pattern_objects import ObservationExpression, ComparisonExpression, \
     ComparisonExpressionOperators, ComparisonComparators, Pattern, \
-    CombinedComparisonExpression, CombinedObservationExpression, ObservationOperators
+    CombinedComparisonExpression, CombinedObservationExpression, ObservationOperators, StartStopQualifier
 from stix_shifter.stix_translation.src.utils.transformers import TimestampToMilliseconds
 from stix_shifter.stix_translation.src.json_to_stix import observable
+from datetime import datetime, timedelta
 import os.path as path
 import json
 # import logging
@@ -18,6 +19,7 @@ import re
 # logger = logging.getLogger(__name__)
 
 PROTOCOL_LOOKUP_JSON_FILE = 'json/network_protocol_map.json'
+START_STOP_PATTERN = r"\d{4}(-\d{2}){2}T\d{2}(:\d{2}){2}(\.\d+)?Z"
 
 class QueryStringPatternTranslator:
     # Change comparator values to match with supported data source operators
@@ -40,14 +42,19 @@ class QueryStringPatternTranslator:
         ObservationOperators.And: 'OR'
     }
 
-    def __init__(self, pattern: Pattern, data_model_mapper, log_type):
+    def __init__(self, pattern: Pattern, data_model_mapper, log_type, time_range):
         self.dmm = data_model_mapper
         self.pattern = pattern
+        self._time_range = time_range
         self._log_config_master_file = self._master_log_file(log_type)
         self._log_config_data = self.load_json(self._log_config_master_file)
         self._protocol_lookup_needed = True if log_type == 'vpcflow' else False
         self._parse_list = []
         self.qualified_queries = []
+        self.time_range_lst = []
+        self._exclude_non_match_lst = []
+        # self._is_single_comp_exp = True
+        self._filter_string_exclude = ''
         self._master_query_reference = self._log_config_data.get('master_query_reference')
         self.translated = self.parse_expression(pattern)
 
@@ -186,7 +193,8 @@ class QueryStringPatternTranslator:
 
     # @staticmethod
     def _parse_mapped_fields(self, expression, value, comparator, stix_field, mapped_fields_array):
-        comparison_string = ""
+        # comparison_string, _exclude_non_match_qry = "", ""
+        comparison_string =  ""
         mapped_fields_count = len(mapped_fields_array)
         for mapped_field in mapped_fields_array:
             if mapped_field in self._log_config_data.get('filter_mapping'):
@@ -238,14 +246,56 @@ class QueryStringPatternTranslator:
                                                                                       value=value)
                 if 'field_mapping' in self._log_config_data:
                     self._parse_list.append(self._log_config_data.get('field_mapping').get(mapped_field))
+
+                self._exclude_non_match_lst.append('strlen({}) > 0'.format(self._log_config_data.get(
+                    'filter_mapping').get(mapped_field)))
                 if mapped_fields_count > 1:
                     comparison_string += " OR "
+                    # _exclude_non_match_qry += " OR "
                     mapped_fields_count -= 1
             else:
                 raise NotImplementedError(
                     "{} unsupported for AWS log {}".format(stix_field, path.basename(
                         self._log_config_master_file).split('_')[0]))
+            # self._exclude_non_match_lst.append('strlen({}) > 0'.format(self._log_config_data.get(
+            #     'filter_mapping').get(mapped_field)))
+        # return comparison_string, _exclude_non_match_qry
         return comparison_string
+
+    # @staticmethod
+    def _parse_time_range(self, qualifier, time_range):
+        """
+        Format the input time range
+        i.e <START|STOP>t'2019-04-20T10:43:10.003Z to %d %b %Y %H:%M:%S %z"(i.e 23 Oct 2018 12:20:14 +0000)
+        :param qualifier: str | None, input time range i.e START t'2019-04-10T08:43:10.003Z'
+        STOP t'2019-04-20T10:43:10.003Z'
+        :param stix_obj: str, file or process stix object
+        :param relevance_map_dict: dict, relevance property format string
+        :param time_range: int, value available from main.py in options variable
+        :return: str, format_string bound with time range provided
+        """
+        format_string = ''
+        # format_string_list = []
+        # epoch_time_string = "01 Jan 1970 00:00:00 +0000"
+        # qualifier_keys_list = ['mapped_field', 'extra_mapped_string', 'transformer', 'default_if_attr_undefined']
+        try:
+            compile_timestamp_regex = re.compile(START_STOP_PATTERN)
+            transformer = TimestampToMilliseconds()
+            if qualifier and compile_timestamp_regex.search(qualifier):
+                time_range_iterator = map(lambda x: int(transformer.transform(x.group())/1000),
+                                          compile_timestamp_regex.finditer(qualifier))
+            # Default time range Start time = Now - 5 minutes and Stop time  = Now
+            else:
+                stop_time = datetime.now()
+                start_time = int(round((stop_time - timedelta(minutes=time_range)).timestamp()))
+                stop_time = int(round(stop_time.timestamp()))
+                # time_range_iterator = map(lambda x: transformer.transform(x),
+                #                           [start_time, stop_time])
+                time_range_iterator = [start_time, stop_time]
+            self.time_range_lst.append([each for each in time_range_iterator])
+            return format_string
+        except (KeyError, IndexError, TypeError) as e:
+            raise e
 
     @staticmethod
     def _is_reference_value(stix_field):
@@ -278,9 +328,9 @@ class QueryStringPatternTranslator:
             # Resolve the comparison symbol to use in the query string (usually just ':')
             comparator = self._lookup_comparison_operator(self, expression.comparator)
 
-            if stix_field == 'start' or stix_field == 'end':
-                transformer = TimestampToMilliseconds()
-                expression.value = transformer.transform(expression.value)
+            # if stix_field == 'start' or stix_field == 'end':
+            #     transformer = TimestampToMilliseconds()
+            #     expression.value = transformer.transform(expression.value)
 
             # Some values are formatted differently based on how they're being compared
             if expression.comparator == ComparisonComparators.Matches:  # needs forward slashes
@@ -288,7 +338,8 @@ class QueryStringPatternTranslator:
             # should be (x, y, z, ...)
             elif expression.comparator == ComparisonComparators.In:
                 value = self._format_set(expression.value)
-            elif expression.comparator == ComparisonComparators.Equal or expression.comparator == ComparisonComparators.NotEqual:
+            elif expression.comparator == ComparisonComparators.Equal or \
+                    expression.comparator == ComparisonComparators.NotEqual:
                 # Should be in single-quotes
                 value = self._format_equality(expression.value)
             # '%' -> '*' wildcard, '_' -> '?' single wildcard
@@ -298,63 +349,110 @@ class QueryStringPatternTranslator:
                 # value = self._escape_value(expression.value)
                 value = '"{}"'.format(expression.value)
 
-            comparison_string = self._parse_mapped_fields(expression, value, comparator,
-                                                                        stix_field, mapped_fields_array)
+            # comparison_string, _exclude_non_match_qry = self._parse_mapped_fields(expression, value, comparator,
+            #                                                                       stix_field, mapped_fields_array)
+            comparison_string = self._parse_mapped_fields(expression, value, comparator, stix_field,
+                                                          mapped_fields_array)
             if len(mapped_fields_array) > 1:
                 # More than one data source field maps to the STIX attribute, so group comparisons together.
                 grouped_comparison_string = "(" + comparison_string + ")"
                 comparison_string = grouped_comparison_string
+                # _exclude_non_match_qry = "(" + _exclude_non_match_qry + ")"
 
             if expression.negated:
                 comparison_string = self._negate_comparison(comparison_string)
-            if qualifier is not None:
-                return "{} {}".format(comparison_string, qualifier)
-            else:
-                return "{}".format(comparison_string)
+            # if qualifier is not None:
+            #     return "{} {}".format(comparison_string, qualifier)
+            # else:
+
+            # self._exclude_non_match_qry = ' OR '.join(self._exclude_non_match_lst)
+            # self._exclude_non_match_lst.append(_exclude_non_match_qry)
+            # if self._is_single_comp_exp:
+            #     self._filter_string_exclude = ''.join(self._exclude_non_match_lst)
+            return "{}".format(comparison_string)
 
         elif isinstance(expression, CombinedComparisonExpression):
+            # self._is_single_comp_exp = False
             operator = self._lookup_comparison_operator(self, expression.operator)
             expression_01 = self._parse_expression(expression.expr1)
             expression_02 = self._parse_expression(expression.expr2)
-            if not expression_01 or not expression_02:
-                return ''
-            if isinstance(expression.expr1, CombinedComparisonExpression):
-                expression_01 = "({})".format(expression_01)
-            if isinstance(expression.expr2, CombinedComparisonExpression):
-                expression_02 = "({})".format(expression_02)
-            query_string = "{} {} {}".format(expression_01, operator, expression_02)
-            if qualifier is not None:
-                return "{} {}".format(query_string, qualifier)
+            if not expression_01:
+                query_string = "{}".format(expression_02)
+            elif not expression_02:
+                query_string = "{}".format(expression_01)
             else:
-                return "{}".format(query_string)
+                query_string = "{} {} {}".format(expression_01, operator, expression_02)
+                # self._filter_string_exclude += ' {expr} '.format(expr=' {} '.format(operator).join(
+                #         self._exclude_non_match_lst) if len(self._exclude_non_match_lst) > 1 else ' {} {}'.
+                #                                                  format(operator,
+                #                                                         ''.join(self._exclude_non_match_lst).strip()))
+                # self._exclude_non_match_lst = []
+            return query_string
+            # if not expression_01 or not expression_02:
+            #     return ''
+            # if isinstance(expression.expr1, CombinedComparisonExpression):
+            #     expression_01 = "({})".format(expression_01)
+            # if isinstance(expression.expr2, CombinedComparisonExpression):
+            #     expression_02 = "({})".format(expression_02)
+            # query_string = "{} {} {}".format(expression_01, operator, expression_02)
+            # # import pdb;pdb.set_trace()
+            # if len(self._exclude_non_match_lst) > 1:
+            #     self._filter_string_exclude += ' {expr} '.format(expr=' {} '.format(operator).join(
+            #         self._exclude_non_match_lst))
+            # else:
+            #     self._filter_string_exclude += operator + ' '.join(self._exclude_non_match_lst)
+            # self._exclude_non_match_lst = []
+            # self._exclude_non_match_qry = ' {operator} '.format(operator=operator.join(self._exclude_non_match_lst))
+            # if qualifier is not None:
+            #     return "{} {}".format(query_string, qualifier)
+            # else:
         elif isinstance(expression, ObservationExpression):
             # return self._parse_expression(expression.comparison_expression, qualifier)
+            # self._exclude_non_match_lst = []
+            self._filter_string_exclude = ''
+            self._parse_list = []
+            self._parse_list = []
             filter_query = self._parse_expression(expression.comparison_expression, qualifier)
             parse_query = '| '.join(self._parse_list) if self._parse_list else ''
+            self._parse_time_range(qualifier, self._time_range)
+            self._filter_string_exclude = ' OR '.join(self._exclude_non_match_lst)
             self.qualified_queries.append(self._master_query_reference.format(parse_query=parse_query,
-                                                                              filter_query=filter_query))
+                                                                              filter_query=filter_query,
+                                                                              exclude_non_match=
+                                                                              self._filter_string_exclude))
+            # self.qualified_queries.append(self._master_query_reference.format(parse_query=parse_query,
+            #                                                                   filter_query=filter_query,
+            #                                                                   ))
             return None
-        elif hasattr(expression, 'qualifier') and hasattr(expression, 'observation_expression'):
-            if isinstance(expression.observation_expression, CombinedObservationExpression):
-                operator = self._lookup_comparison_operator(self, expression.observation_expression.operator)
-                expression_01 = self._parse_expression(expression.observation_expression.expr1)
-                # qualifier only needs to be passed into the parse expression once since it will be the same for both expressions
-                expression_02 = self._parse_expression(expression.observation_expression.expr2, expression.qualifier)
-                return "{} {} {}".format(expression_01, operator, expression_02)
-            else:
-                return self._parse_expression(expression.observation_expression.comparison_expression, expression.qualifier)
+        # For now not needed - will look at different implementation for qualifier
+        # elif hasattr(expression, 'qualifier') and hasattr(expression, 'observation_expression'):
+        #     if isinstance(expression.observation_expression, CombinedObservationExpression):
+        #         operator = self._lookup_comparison_operator(self, expression.observation_expression.operator)
+        #         expression_01 = self._parse_expression(expression.observation_expression.expr1)
+        #         # qualifier only needs to be passed into the parse expression once since it will be the same for both expressions
+        #         expression_02 = self._parse_expression(expression.observation_expression.expr2, expression.qualifier)
+        #         return "{} {} {}".format(expression_01, operator, expression_02)
+        #     else:
+        #         return self._parse_expression(expression.observation_expression.comparison_expression, expression.qualifier)
+
         elif isinstance(expression, CombinedObservationExpression):
-            operator = self._lookup_comparison_operator(self, expression.operator)
-            expression_01 = self._parse_expression(expression.expr1)
-            expression_02 = self._parse_expression(expression.expr2)
-            if expression_01 and expression_02:
-                return "({}) {} ({})".format(expression_01, operator, expression_02)
-            elif expression_01:
-                return "{}".format(expression_01)
-            elif expression_02:
-                return "{}".format(expression_02)
-            else:
-                return ''
+            self._parse_expression(expression.expr1, qualifier)
+            self._parse_expression(expression.expr2, qualifier)
+            return None
+            # operator = self._lookup_comparison_operator(self, expression.operator)
+            # expression_01 = self._parse_expression(expression.expr1)
+            # expression_02 = self._parse_expression(expression.expr2)
+            # if expression_01 and expression_02:
+            #     return "({}) {} ({})".format(expression_01, operator, expression_02)
+            # elif expression_01:
+            #     return "{}".format(expression_01)
+            # elif expression_02:
+            #     return "{}".format(expression_02)
+            # else:
+            #     return ''
+        elif isinstance(expression, StartStopQualifier):
+            if hasattr(expression, 'observation_expression'):
+                return self._parse_expression(getattr(expression, 'observation_expression'), expression.qualifier)
         elif isinstance(expression, Pattern):
             return "{expr}".format(expr=self._parse_expression(expression.expression))
         else:
@@ -368,9 +466,11 @@ class QueryStringPatternTranslator:
 def translate_pattern(pattern: Pattern, data_model_mapping, options):
     # Query result limit and time range can be passed into the QueryStringPatternTranslator if supported by the data source.
     # result_limit = options['result_limit']
-    # timerange = options['timerange']
+    timerange = options['timerange']
     log_type = options['log_type']
-    final_queries = QueryStringPatternTranslator(pattern, data_model_mapping, log_type).qualified_queries
+    final_queries_obj = QueryStringPatternTranslator(pattern, data_model_mapping, log_type, timerange)
+    final_queries = final_queries_obj.qualified_queries
+    qualifier = final_queries_obj.time_range_lst
     # Add space around START STOP qualifiers
     # query = re.sub("START", "START ", query)
     # query = re.sub("STOP", " STOP ", query)
@@ -378,4 +478,4 @@ def translate_pattern(pattern: Pattern, data_model_mapping, options):
     # Change return statement as required to fit with data source query language.
     # If supported by the language, a limit on the number of results may be desired.
     # A single query string, or an array of query strings may be returned
-    return final_queries
+    return final_queries, qualifier
